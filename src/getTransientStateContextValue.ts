67c51ad3e948e5396679fce20be944ac6b8c1c2b
axtk <axtkx@inbox.ru>
2025-04-28 13:23:24 +0300

    remove debugging object from initial context value

--- src/getTransientStateContextValue.ts
+++ src/getTransientStateContextValue.ts
@@ -2,11 +2,5 @@ import type {Store} from 'groundstate';
 import type {TransientState} from './TransientState';
 
 export function getTransientStateContextValue() {
-    let transientState = new Map<string, Store<TransientState>>();
-
-    // for debugging
-    if (typeof window !== 'undefined')
-        window._transientState = transientState;
-
-    return transientState;
+    return new Map<string, Store<TransientState>>();
 }
